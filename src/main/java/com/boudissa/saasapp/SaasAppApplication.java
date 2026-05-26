78135e271ac728daaa0ab1d6f325fbfdf518cceb
soffianeboudissa <soffiane.boudissa@cogis.com>
2026-05-26 16:15:27 +0200

    first commit

---
+++ src/main/java/com/boudissa/saasapp/SaasAppApplication.java
@@ -0,0 +1,13 @@
+package com.boudissa.saasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boudissa/saasapp/SaasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boudissa.saasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
